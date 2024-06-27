f073ffaf7b74e2a7cd0ac92ea28b4715cd216b13
Danieldd96 <danieldelgado.dd270@gmail.com>
2024-06-27 09:47:08 -0600

    instalacion de router y creacion de carrusel de imagenes

---
+++ src/assets/dataGratis.js
@@ -0,0 +1,17 @@
+export const data = [
+    {
+      id: 1, imgUrl: "src/img/Apex game.jpg"
+    },
+    {
+      id: 2, imgUrl: "src/img/Overwatch.jpeg"
+    },
+    {
+      id: 3, imgUrl: "src/img/days gone.jpg"
+    },
+    {
+      id: 4, imgUrl: "src/img/Star wars.jpeg"
+    },
+    {
+      id: 5, imgUrl: "src/img/Coldcod.jpg"
+    }
+  ];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,11 @@
+import Carrusel from "../components/Carrusel"
 const Home = ()=>{
     return(
         <div>
-            <div>
+            <div className="Destacados">
                 <h2>Destacados y recomendados</h2>
-                <ul>
-                    <li><h2></h2></li>
-                    <li><h2></h2></li>
-                    <li><h2></h2></li>
-                    <li><h2></h2></li>
-                    <li><h2></h2></li>
-                    <li><h2></h2></li>
-                </ul>
+                <Carrusel/>
+                
             </div>
             <div>
                 <h2>Juegos en oferta</h2>
