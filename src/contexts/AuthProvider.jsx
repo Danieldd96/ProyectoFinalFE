b2324860bb03fffd863e3efc599725d6575e4e3a
Danieldd96 <danieldelgado.dd270@gmail.com>
2024-07-06 14:22:24 -0600

    Cambios de estructura en el login

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,16 +1,15 @@
-import { createContext,useEffect,useState } from "react";
+import { createContext,useState } from "react";
+
 export const AuthContext = createContext()
+
 const AuthProvider=({children})=>{
     const [auth,setAuth]=useState(false)
     const login = ()=>setAuth(true)
-    const Logout = ()=>setAuth(false)
-    
-    
-return(
-    <AuthContext.Provider value={{auth,login,Logout}}>
-        {children}
-    </AuthContext.Provider>
-)
-
+    const logout = ()=>setAuth(false)
+    return(
+        <AuthContext.Provider value={{auth,login,logout}}>
+            {children}
+        </AuthContext.Provider>
+    )
 }
 export default AuthProvider
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom"
-import { useContext, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import { Get } from "../hooks/Get"
 import { AuthContext } from "../contexts/AuthProvider"
@@ -9,37 +9,25 @@ const Login = ()=>{
     const [email, setEmail] = useState("");        ///Con este UseState obtendre el contenido del input en el cual se obtendra el email
     const [clave, setClave] = useState("");        ///Con este UseState obtendre el contenido del input en el cual se obtendra el la contraseña
     const {login} = useContext(AuthContext);       ///Con este UseContext verificare cuales rutas seran privadas
-
+    const [datos,setDatos]= useState("")
     let apiUrl="http://localhost:3001/users/"      ///Api que contendra los datos de los usuarios que se hayan registrado previamente
+    useEffect(()=>{
+      obtener()
+    },[])
+    async function obtener() {
+      const data= await Get(apiUrl)
+       setDatos(data)
+    }
     const Guardar = async () => {
-      try {
-        const usuarios = await Get(apiUrl);        ///usuarios obtendra los usuarios guardados de en la api
-        
-        usuarios.forEach((usuario) => {               ///Este forEach recorrera los datos del api
-          const emailRegistrado = usuario.email;      ///Este const contendra el email de la api      
-          const passRegistrado = usuario.pass;        ///Este const contendra la contraseña de la api   
-          const idRegistrado = usuario.id;            ///Este const contendra el id del usuario de la api   
-          const userName = usuario.user;              ///Este const contendra el usuario de la api   
-          console.log(emailRegistrado);
-          console.log(passRegistrado);
-          console.log(idRegistrado);
-          if (email === emailRegistrado && clave === passRegistrado) {  ///Este if vera si el email y clave previamente guardado en el useState es igual al guardado en la api
-            console.log("Usuario encontrado");
-            localStorage.setItem("email", emailRegistrado);      ///Con local storage guardaremos el email de la api en "email" del local storge para usarlo en otras funciones
-            localStorage.setItem("idUsuario", idRegistrado);     ///Con local storage guardaremos el Id de la api en "idUsuario" del local storge para usarlo en otras funciones
-            localStorage.setItem("usuario", userName);           ///Con local storage guardaremos el Nombre de usuario de la api en "usuario" del local storge para usarlo en otras funciones
-            setTimeout(() => {      ///Este setTimeout hara que la funcion pasado un segundo use el UseNavigate para trasladarnos a la pagina principal y llamaremos a la funcion login
-              navegar("/");
-              login();
-            }, 1000);
-          }else{
-            alert("Email o contraseña incorrectos");
-          }
-        });
-      } catch (error) {
-        console.log(error);
-        alert("Error al iniciar sesión");
-      }
+       const user=datos.find((user)=>user.email==email)
+       console.log("Usuario encontrado",user)
+       if (user.pass==clave) {
+        navegar('/')
+        login()
+       }else{
+        console.log("su usuario fue autenticado correctamente");
+        obtener()
+       }
     };
     return(
         <div className="register" >
